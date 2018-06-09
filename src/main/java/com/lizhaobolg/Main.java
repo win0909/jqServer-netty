70b4a62ee4480328a39067b52483ac095cdc1fc6
lizhao <3060507060@qq.com>
2018-06-09 15:53:17 +0800

    init

---
+++ src/main/java/com/lizhaobolg/Main.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2015-2018
+ * FileName: Main
+ * Author:   zhao
+ * Date:     2018/6/9 15:36
+ * Description: 主入口
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.lizhaobolg;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈主入口〉
+ *
+ * @author zhao
+ * @create 2018/6/9
+ * @since 1.0.0
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("HelloWorld");
+  }
+}
