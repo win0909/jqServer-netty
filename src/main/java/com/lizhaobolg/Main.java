64d23472a614209d69e3b7378cbdeb01298a2ddd
lizhao <3060507060@qq.com>
2018-06-10 21:56:20 +0800

    添加netty及测试类

--- src/main/java/com/lizhaobolg/Main.java
+++ src/main/java/com/lizhaobolg/Main.java
@@ -10,6 +10,7 @@
  */
 package com.lizhaobolg;
 
+import com.lizhaobolg.net.SocketServer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,8 +26,9 @@ import org.slf4j.LoggerFactory;
 public class Main {
   private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
+  public static void main(String[] args) throws Exception {
+    logger.info("开始启动Socket服务器...");
+    new SocketServer().run();
 
-  public static void main(String[] args) {
-    logger.info("HelloWorld");
   }
 }
